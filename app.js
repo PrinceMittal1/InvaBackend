60466b4ebf422e0389bb410f62e43a1d6ef94cd4
Prince <prince.mittal@sachtechsolution.com>
2025-09-19 14:40:39 +0530

    some changes

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const cors = require("cors");
 
 dotenv.config();
 
+app.use(cors());
 const app = express();
 app.use(express.json());
 
